12a6adac9ce1d94115417d2a4ceee595b6ebc946
Hemant Chaurasia <62471000+hemantchaurasia2002@users.noreply.github.com>
2022-05-02 18:18:41 +0530

    3rd commit

---
+++ ReverseStringByStack.c
@@ -0,0 +1,17 @@
+//string reversal using stack
+#include <iostream>
+
+void Reverse(char *c, int n)
+{
+
+}
+
+int main()
+{
+  char C[51];
+  printf("Enter a string");
+  gets(C);
+  Reverse(C,strlen(C));
+  printf("Output = %s,C");
+  
+}
