2b21d763e14683e755d9e4e808f0e9b1b211b4bf
Hemant Chaurasia <62471000+hemantchaurasia2002@users.noreply.github.com>
2022-05-02 10:16:28 +0530

    Create StructLL.c

---
+++ StructLL.c
@@ -0,0 +1,25 @@
+//Program of STACK using LINKEDLIST
+#include <stdio.h>
+
+struct Node 
+{
+  int data;
+  struct Node* link;
+};
+
+struct Node* top = NULL;
+
+void Push(int x){
+  struct Node* temp = (struct Node*)malloc(sizeof(struct Node*));
+  temp->data = x;
+  temp->link = top;
+  top = temp;
+}
+
+void Pop(){
+  struct Node *temp;
+  if(top == NULL) return;
+  temp = top;
+  top = top->link;
+  free(temp);
+}
