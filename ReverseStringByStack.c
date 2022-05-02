26bdb2ba91d9053b05f901767b3dd6008ec8679b
Hemant Chaurasia <62471000+hemantchaurasia2002@users.noreply.github.com>
2022-05-02 18:32:07 +0530

    3.1 commit

--- ReverseStringByStack.c
+++ ReverseStringByStack.c
@@ -1,9 +1,18 @@
 //string reversal using stack
-#include <iostream>
+#include <stdio.h>
 
 void Reverse(char *c, int n)
 {
-
+  stack<char> S;
+  //loop for push
+  for(int i=0; i<n; i++){
+    S.push(C[i]);
+  }
+  //loop for pop
+  for(int i=0; i<n; i++){
+    C[i]=S.top();
+    S.pop();
+  }
 }
 
 int main()
@@ -13,5 +22,5 @@ int main()
   gets(C);
   Reverse(C,strlen(C));
   printf("Output = %s,C");
-  
+
 }
